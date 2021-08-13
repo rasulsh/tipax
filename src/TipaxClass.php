00f34d4dac6d20b5ea9bf311878b5304de2424e4
rasulsh <me@rasulsh.ir>
2021-08-13 12:59:15 +0430

    implement login method

---
+++ src/TipaxClass.php
@@ -0,0 +1,83 @@
+<?php
+
+class Tipax {
+    private $api_url;
+    private $username;
+    private $password;
+    private $system_token;
+
+
+
+    public function __construct() {
+    }
+
+    // Setter methods
+
+    public function setApiUrl($api_url) {
+        $this->api_url = $api_url;
+    }
+
+    public function setUsername($username) {
+        $this->username = $username;
+    }
+
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+
+    public function setSystemToken($system_token) {
+        $this->system_token = $system_token;
+    }
+
+    // Getter methods
+
+    public function getApiUrl() {
+        return $this->api_url;
+    }
+
+    public function getUsername() {
+        return $this->username;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function getSystemToken() {
+        return $this->system_token;
+    }
+
+    // Class Body 
+
+    public function login() {
+        $ch = curl_init();
+
+        $data = json_encode([
+            'SystemToken' => $this->getSystemToken(),
+            'Item' => [
+                'UserName' => $this->getUsername(),
+                'Password' => $this->getPassword()
+            ]
+        ]);
+
+
+        curl_setopt($ch, CURLOPT_URL, $this->getApiUrl() . '/Engine/Login');
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+        ));
+
+        $response = curl_exec($ch);
+        $info     = curl_getinfo($ch);
+        curl_close($ch);
+
+        return json_decode(json_encode([
+            'status' => $info['http_code'],
+            'data'   => json_decode($response)
+        ]));
+    }
+
+}
