69547645907b9a0b0fa992fbf5563876275f891f
rasulsh <me@rasulsh.ir>
2021-08-14 12:24:23 +0430

    implement registerContract method

--- src/TipaxClass.php
+++ src/TipaxClass.php
@@ -367,4 +367,82 @@ class Tipax {
             die("HTTP Error : " . $login->status);
         }
     }
+
+	public function registerContract() {
+        $login = $this->login();
+
+        if ($login->status === 200) {
+            if (!$login->data->StatusBase) {
+
+                $ch = curl_init();
+
+                $data = json_encode([
+                    'SystemToken'   => $this->getSystemToken(),
+                    "UserToken"     => $login->data->Item->Token,
+                    'Item'          => [
+                        "Contract"  => [
+                            "KindID"    => 0,
+                            "OrderNumber"   => "2185"
+                        ],
+                        "Sender"    => [
+                            "PersonKindID"  => 1,
+                            "Name"          => "rasul",
+                            "LastName"      => "sh",
+                            "Mobile"        => "09129339790",
+                            "CityID"        => 36,
+                            "Address"       => "isfahan",
+                            "Lat"           => 0,
+                            "Long"          => 0
+                        ],
+                        "Receiver"  => [
+                            "PersonKindID"  => 1,
+                            "Name"          => "rasul",
+                            "LastName"      => "sh",
+                            "Mobile"        => "09129339790",
+                            "CityID"        => 57,
+                            "Address"       => "isfahan",
+                            "Lat"           => 0,
+                            "Long"          => 0
+                        ],
+                        "Dispatchs" => [
+                            [
+                                "GoodKindID"    => "1",
+                                "PackageTypeID" => "1",
+                                "Weight"        => "100", // بر حسب کیلوگرم و حداقل باید یک کیلوگرم باشه
+                                "Length"        => 0,
+                                "Width"         => 0,
+                                "Height"        => 0
+                            ]
+                        ],
+                        "Amounts" => [
+                            "DispatchValue" => "50000", // بر حسب ریال
+                            "OnlineSales"   => 0 // بر حسب ریال
+                        ]
+                    ]
+                ]);
+
+                curl_setopt($ch, CURLOPT_URL, $this->getApiUrl() . '/Engine/RegisterContract');
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                    'Content-Type: application/json',
+                ));
+
+                $response = curl_exec($ch);
+                $info     = curl_getinfo($ch);
+                curl_close($ch);
+
+                return json_decode(json_encode([
+                    'status' => $info['http_code'],
+                    'data'   => json_decode($response)
+                ]));
+            } else {
+                die("Web Service Error : " . $login->data->Message);
+            }
+        } else {
+            die("HTTP Error : " . $login->status);
+        }
+    }
 }
